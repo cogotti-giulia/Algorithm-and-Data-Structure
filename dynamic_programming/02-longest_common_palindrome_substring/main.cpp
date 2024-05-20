d66d04b0b359e7467d51461d8c62445dcd855de0
cogotti-giulia <cogotti.giulia.irl@gmail.com>
2024-05-20 11:31:01 +0200

    [DYNA PROGR] update lcspsubs

--- dynamic_programming/02-longest_common_palindrome_substring/main.cpp
+++ dynamic_programming/02-longest_common_palindrome_substring/main.cpp
@@ -5,38 +5,37 @@
 
 int lcpsubs_top_down_AUX(std::string X, std::vector<std::vector<int>> &lung,
                          int i, int j) {
-  
+
   if (i > j) // stringa vuota
     return 0;
+
   if (i == j) // un carattere in comune
     lung[i][j] = 1;
 
-  if (lung[i][j] == 0) {      // se non ho ancora calcolato il valore
-    if (X.at(i) == X.at(j)) { // se trovo due caratteri uguali
-      int inner_string =
-          j - i -
-          1; // controllo la porzione interna, andando in ricorsione (i+1 e j-1)
+  if (j > i && lung[i][j] == 0) { // se non ho ancora calcolato il valore
+    if (X.at(i) == X.at(j)) {     // se trovo due caratteri uguali
+      // controllo la porzione interna, andando in ricorsione (i+1 e j-1)
+      int inner_string = j - i - 1;
 
       if (inner_string == lcpsubs_top_down_AUX(X, lung, i + 1, j - 1)) {
-        lung.at(i).at(j) =
-            2 + inner_string; // se tutta la stringa interna è palindroma
-                              // incremento il valore in i j del numero delle
-                              // lettere che compongono la sotto stringa (il 2 è
-                              // per le due lettere xi e xj)
+        // se tutta la stringa interna è palindroma
+        // incremento il valore in i j del numero delle
+        // lettere che compongono la sotto stringa (il 2 è
+        // per le due lettere xi e xj)
+        lung.at(i).at(j) = 2 + inner_string;
+
         return lung.at(i).at(j); // ok ho finito
       }
     }
-
-    lung.at(i).at(j) = std::max(
-        lcpsubs_top_down_AUX(X, lung, i, j - 1),
-        lcpsubs_top_down_AUX(
-            X, lung, i + 1,
-            j)); // altrimenti vado alla ricerca delle massime sotto stringhe
-                 // palindrome decrementando gli indici esterni
+    // altrimenti vado alla ricerca delle massime sotto stringhe
+    // palindrome decrementando gli indici esterni
+    lung.at(i).at(j) = std::max(lcpsubs_top_down_AUX(X, lung, i, j - 1),
+                                lcpsubs_top_down_AUX(X, lung, i + 1, j));
   }
 
   return lung.at(i).at(j);
 }
+
 void lcpsubs_top_down(std::string X) {
   int n = X.size();
 
